66b5bdfb0adbce3018ecd22e4061af7b012c4ebd
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-09-08 22:22:51 -0300

    :dizzy: chore: carousel loop

--- components/Home/components/CategoryBookSection/components/CarouselAutoplay/index.tsx
+++ components/Home/components/CategoryBookSection/components/CarouselAutoplay/index.tsx
@@ -4,7 +4,6 @@ import { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
 
 export const CarouselAutoplay = () => {
   return (
--- components/Home/components/TreendingBooksSection/components/Carousel/index.tsx
+++ components/Home/components/TreendingBooksSection/components/Carousel/index.tsx
@@ -1,16 +1,21 @@
 import { BookCover } from '../../../../../BookCover';
 
+import { Autoplay } from 'swiper';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 export const Carousel = () => {
   return (
     <Swiper
+      modules={[Autoplay]}
       spaceBetween={30}
       slidesPerView={'auto'}
-      centeredSlides={true}
       grabCursor={true}
       initialSlide={4}
+      loop={true}
+      autoplay={{ delay: 0, disableOnInteraction: false }}
+      speed={2000}
       style={{
         width: '100%',
         height: '320px',

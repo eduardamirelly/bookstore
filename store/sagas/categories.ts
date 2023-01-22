54d2f008d6e461a1f55d443eb925a70216e2ef10
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-22 17:36:40 -0300

    :art: get categories from api

--- store/sagas/categories.ts
+++ store/sagas/categories.ts
@@ -2,15 +2,14 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 import { loadCategoriesRequest, loadCategoriesFailure, loadCategoriesSuccess } from '../features/categories';
 import api from '../../services/api';
 import Category from '../@types/categories';
+import { AxiosResponse } from 'axios';
 
 const getCategories = () => api.get<Category[]>('/categories');
 
 function* fetchCategories() {
-  console.log(process.env.API_URL)
   try {
-    const categories: Category[] = yield call(getCategories);
-    console.log(categories);
-    yield put({ type: loadCategoriesSuccess.type, categories });
+    const response: AxiosResponse<Category[]> = yield call(getCategories);
+    yield put({ type: loadCategoriesSuccess.type, payload: response.data });
   } catch {
     yield put({ type: loadCategoriesFailure.type });
   }

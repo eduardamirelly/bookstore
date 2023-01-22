a758434e2b4f4a35311a06030d1939d674aa9d9b
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-22 17:22:21 -0300

    :wrench: update .env.example and test dispatch to get the categories

--- store/sagas/categories.ts
+++ store/sagas/categories.ts
@@ -1,20 +1,20 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { loadCategoriesRequest, loadCategoriesFailure } from '../features/categories';
+import { loadCategoriesRequest, loadCategoriesFailure, loadCategoriesSuccess } from '../features/categories';
 import api from '../../services/api';
 import Category from '../@types/categories';
 
 const getCategories = () => api.get<Category[]>('/categories');
 
 function* fetchCategories() {
+  console.log(process.env.API_URL)
   try {
     const categories: Category[] = yield call(getCategories);
     console.log(categories);
-    yield put({ type: loadCategoriesRequest, categories });
+    yield put({ type: loadCategoriesSuccess.type, categories });
   } catch {
-    yield put({ type: loadCategoriesFailure });
+    yield put({ type: loadCategoriesFailure.type });
   }
 }
 
-export default [
-  takeLatest(loadCategoriesRequest.type, fetchCategories)
-]
+export default takeLatest(loadCategoriesRequest.type, fetchCategories)
+

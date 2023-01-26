7ac49cde40b30deae307f9ebfb77adf61774d4ea
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-25 21:33:37 -0300

    :art: click to mark the favorite button

--- components/BookMark/index.tsx
+++ components/BookMark/index.tsx
@@ -1,9 +1,19 @@
 import { BookmarkSimple } from 'phosphor-react';
+import { useState } from 'react';
 import { MarkStyle } from './styles';
 
-export const BookMark = () => {
+interface BookMarkProps {
+  isFavorited?: boolean;
+}
+
+export const BookMark = ({ isFavorited }: BookMarkProps) => {
+  const [isMarked, setIsMarked] = useState(isFavorited);
+
   return (
-    <MarkStyle>
+    <MarkStyle
+      mark={isMarked ? 'active' : 'off'}
+      onClick={() => setIsMarked(!isMarked)}
+    >
       <BookmarkSimple size={28} weight="regular" />
     </MarkStyle>
   );

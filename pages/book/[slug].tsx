d748fbeddb140d5ceac936dcff7a78ebf4b97dc4
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-28 21:46:53 -0300

    :art: get books state on Book page

--- pages/book/[slug].tsx
+++ pages/book/[slug].tsx
@@ -1,16 +1,32 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BookDetail } from '../../components/BookDetail';
 import { Footer } from '../../components/Footer';
 import { CategoryBookSection } from '../../components/Home/components/CategoryBookSection';
 import { Navbar } from '../../components/Navbar';
+import { useReduxSelector } from '../../store';
+import { loadBooksRequest } from '../../store/features/books';
 import { Container } from '../../styles/Container';
 
 const Book: NextPage = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const books = useReduxSelector((state) => state.books.data);
+  const bookDetail = books.find((book) => book.id === slug);
+
+  useEffect(() => {
+    dispatch(loadBooksRequest());
+  }, []);
+
   return (
     <>
       <Container>
         <Navbar />
-        <BookDetail />
+        <BookDetail book={bookDetail} />
         <CategoryBookSection category="View More" />
         <Footer />
       </Container>

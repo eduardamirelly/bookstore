e2b7ba304cb533400846d42de6aabd6f54919f0d
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-30 20:38:07 -0300

    :art: state on CountBooks component

--- components/CountBooks/index.tsx
+++ components/CountBooks/index.tsx
@@ -1,17 +1,20 @@
 import { MinusCircle, PlusCircle } from 'phosphor-react';
+import { useState } from 'react';
 import { Box } from '../../styles/Box';
 import { ButtonCount, CountBox } from './styles';
 
 export const CountBooks = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <Box css={{ gap: '10px', alignItems: 'center' }}>
-      <ButtonCount>
+      <ButtonCount onClick={() => (count > 0 ? setCount(count - 1) : () => {})}>
         <MinusCircle size={24} weight="regular" />
       </ButtonCount>
 
-      <CountBox>00</CountBox>
+      <CountBox>{`${count < 10 ? '0' : ''}${count}`}</CountBox>
 
-      <ButtonCount>
+      <ButtonCount onClick={() => setCount(count + 1)}>
         <PlusCircle size={24} weight="regular" />
       </ButtonCount>
     </Box>

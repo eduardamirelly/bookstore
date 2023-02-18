116353f99cbb294ef152283e63237cee4be00964
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-17 21:22:15 -0300

    🎨 update Link on Navbar

--- components/Navbar/components/BookmarkButton/index.tsx
+++ components/Navbar/components/BookmarkButton/index.tsx
@@ -5,11 +5,9 @@ import { IconClick } from '../../../../styles/IconClick';
 export const BookmarkButton = () => {
   return (
     <Link href="/favorites">
-      <a>
-        <IconClick>
-          <BookmarkSimple size={32} weight="light" />
-        </IconClick>
-      </a>
+      <IconClick>
+        <BookmarkSimple size={32} weight="light" />
+      </IconClick>
     </Link>
   );
 };

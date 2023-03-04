4d78912904061d61ff0ab46faacc0a66bfc3eda9
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-03-03 21:41:08 -0300

    🎨 add variants to Box component

--- styles/Box/index.ts
+++ styles/Box/index.ts
@@ -2,4 +2,28 @@ import { styled } from "../stitches.config";
 
 export const Box = styled('div', {
   display: 'flex',
+
+  variants: {
+    direction: {
+      row: {
+        flexDirection: 'row',
+      },
+      column: {
+        flexDirection: 'column',
+      },
+    },
+    justify: {
+      center: {
+        justifyContent: 'center',
+      },
+      between: {
+        justifyContent: 'space-between',
+      },
+    },
+    align: {
+      center: {
+        alignItems: 'center',
+      },
+    },
+  },
 });

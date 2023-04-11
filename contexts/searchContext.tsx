5074ed4f4751ec14485ca84fffce14ae47b2166f
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-10 21:34:47 -0300

    🚧 update searchContext and apply on Layout

--- contexts/searchContext.tsx
+++ contexts/searchContext.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 
 interface SearchContextType {
-
+  search: string;
+  handleInputText: (value: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const SearchContext = createContext<SearchContextType>({} as SearchContextType);
@@ -11,8 +12,17 @@ interface SearchContextProviderProps {
 }
 
 export function SearchContextProvider({ children }: SearchContextProviderProps) {
+  const [search, setSearch] = useState('');
+
+  const handleInputText = (value: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(value.target.value);
+  }
+
   return (
-    <SearchContext.Provider value={{}}>
+    <SearchContext.Provider value={{
+      search,
+      handleInputText
+    }}>
       {children}
     </SearchContext.Provider>
   )

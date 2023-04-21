3b4266bd2ea8d778067003c8dbec794498eba892
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-21 20:37:19 -0300

    🚧 update catalog page

--- pages/catalog/index.tsx
+++ pages/catalog/index.tsx
@@ -1,25 +1,12 @@
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { CatalogBooks } from '../../components/CatalogBooks';
-import { useReduxSelector } from '../../store';
-import { loadBooksRequest } from '../../store/features/books';
-import { loadCategoriesRequest } from '../../store/features/categories';
 import Layout from '../../templates/Layout';
 
 const Catalog: NextPage = () => {
-  const dispatch = useDispatch();
-  const books = useReduxSelector((state) => state.books.data);
-
-  useEffect(() => {
-    dispatch(loadBooksRequest());
-    dispatch(loadCategoriesRequest());
-  }, []);
-
   return (
     <>
       <Layout>
-        <CatalogBooks isFavorites={false} books={books} />
+        <CatalogBooks isFavorite={false} />
       </Layout>
     </>
   );

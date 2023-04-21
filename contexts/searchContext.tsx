beca69185f658327da606cfbe356ea59b02421d1
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-21 20:36:58 -0300

    🚧 update search context

--- contexts/searchContext.tsx
+++ contexts/searchContext.tsx
@@ -1,12 +1,18 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 import { useReduxSelector } from "../store";
+import { Book } from "../store/@types/books";
+import { loadBooksRequest } from "../store/features/books";
+import { loadCategoriesRequest } from "../store/features/categories";
+import { useDispatch } from "react-redux";
 
 interface SearchContextType {
   search: string;
   orderBy: string;
-  handleInputText: (value: React.ChangeEvent<HTMLInputElement>) => void;
+  booksFiltered: Book[];
+  handleInputText: (value: string) => void;
   handleOrderBy: (value: string) => void;
   handleSortBy?: () => void;
+  handleSetBooksFiltered: (value: Book[]) => void;
 }
 
 export const SearchContext = createContext<SearchContextType>({} as SearchContextType);
@@ -16,13 +22,44 @@ interface SearchContextProviderProps {
 }
 
 export function SearchContextProvider({ children }: SearchContextProviderProps) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadBooksRequest());
+    dispatch(loadCategoriesRequest());
+  }, []);
+
   const books = useReduxSelector((state) => state.books.data);
 
+  const [booksFiltered, setBooksFiltered] = useState<Book[]>(books);
   const [search, setSearch] = useState('');
   const [orderBy, setOrderBy] = useState('');
 
-  const handleInputText = (value: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(value.target.value);
+  const handleInputText = (value: string) => {
+    setSearch(value);
+
+    if (search) {
+      setBooksFiltered(
+        books.filter((item) => {
+          let matchEvent = false;
+
+          Object.values(item).map((valueItem) => {
+            let value = ['string', 'number'].includes(typeof valueItem) ? String(valueItem).toLowerCase() : null;
+            if (value && value.match(search.toLowerCase())) {
+              matchEvent = true;
+            }
+          });
+
+          if (matchEvent) {
+            return item;
+          }
+        })
+      )
+    }
+  }
+
+  const handleSetBooksFiltered = (value: Book[]) => {
+    setBooksFiltered(value);
   }
 
   const handleOrderBy = (value: string) => {
@@ -33,8 +70,10 @@ export function SearchContextProvider({ children }: SearchContextProviderProps) 
     <SearchContext.Provider value={{
       search,
       orderBy,
+      booksFiltered,
       handleInputText,
-      handleOrderBy
+      handleOrderBy,
+      handleSetBooksFiltered,
     }}>
       {children}
     </SearchContext.Provider>

82d7888ff5b9192e2d26b4d649702fc9f2e14e1b
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-21 20:37:30 -0300

    🚧 update favorites page

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,27 +1,13 @@
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { CatalogBooks } from '../../components/CatalogBooks';
-import { useReduxSelector } from '../../store';
-import { loadBooksRequest } from '../../store/features/books';
-import { loadCategoriesRequest } from '../../store/features/categories';
 import Layout from '../../templates/Layout';
 
 const Favorites: NextPage = () => {
-  const dispatch = useDispatch();
-  const books = useReduxSelector((state) => state.books.data);
-
-  useEffect(() => {
-    dispatch(loadBooksRequest());
-    dispatch(loadCategoriesRequest());
-  }, []);
-
   return (
     <>
       <Layout>
         <CatalogBooks
-          isFavorites={true}
-          books={books.filter((book) => book.isFavorite)}
+          isFavorite={true}
         />
       </Layout>
     </>
